3503a7cb325eaf3548a24901e904a4d58f7f8891
Michael Parker <docmparker@gmail.com>
2023-12-21 19:51:18 -0500

    Update combining themes for new output with reasoning

--- src/survey_analysis/theme_derivation.py
+++ src/survey_analysis/theme_derivation.py
@@ -39,6 +39,16 @@ class CommentBatch(InputModel, BaseModel):
         """Shuffles the comments"""
         random.shuffle(self.comments)
 
+class ThemeConsolidation(OpenAISchema, InputModel):
+    """Store the results from the process of combining a list of themes derived from a batch of comments"""
+    step1_reasoning: str = Field("", description="The reasoning for combining themes")
+    step2_intermediate_themes: DerivedThemes = Field(DerivedThemes(), description="The intermediate themes after combining similar themes")
+    final_combined_themes: list[Theme] = Field([], description="The final list of all consolidated themes")
+
+    def is_empty(self) -> bool:
+        """Returns True if all themes are empty"""
+        return len(self.final_combined_themes) == 0
+
 
 class DeriveThemes(SurveyTaskProtocol):
     """Class for deriving themes from a batch of comments"""
@@ -77,13 +87,16 @@ supporting this theme. Each quote should have enough context to be understood. \
 Do not add or alter words in the quotes under any circumstances. If there are \
 less than 3 quotes, then include as many as you can."""
 
-        comment_list_del = "\n".join([f"{delimiter}{comment.comment}{delimiter}" for comment in task_input.comments if not comment.is_empty()])
+        comment_list_del = "\n".join([f"{delimiter}{comment.comment}{delimiter}" 
+                                      for comment in task_input.comments 
+                                      if not comment.is_empty()])
         
         user_message = f"""{comment_list_del}"""
 
-        messages =  [
-        {'role':'system', 'content': system_message},
-        {'role':'user', 'content': user_message}]
+        messages = [
+            {'role': 'system', 'content': system_message},
+            {'role': 'user', 'content': user_message}
+        ]
 
         return messages
     
@@ -93,37 +106,34 @@ less than 3 quotes, then include as many as you can."""
         return DerivedThemes
 
 
-
-# needs an input class that is a list of DerivedThemes
-
-class DerivedThemesBatch(InputModel, BaseModel):
-    """Wraps a batch of derived themes. Used by tasks that take a batch of derived themes."""
-    derived_themes: list[DerivedThemes] = Field([], description="A list of derived themes")
-
-    def is_empty(self) -> bool:
-        """Returns True if all derived themes are empty"""
-        return all([derived_theme.is_empty() for derived_theme in self.derived_themes])
-
-
 async def derive_themes(task_input: CommentBatch, survey_task: SurveyTaskProtocol, shuffle_passes=3) -> DerivedThemes:
     """Derives themes from a batch of comments, coordinating
     multiple shuffled passes to avoid LLM positional bias and
     then combining the results of each pass into a single result"""
     # run survey_task on task_input shuffle_passes times and combine results
     results: list[DerivedThemes] = []
-    for _ in range(shuffle_passes):
+    for i in range(shuffle_passes):
         # shuffle the comments
+        print(f"shuffle pass {i}")
         task_input.shuffle() # trying shuffling to see if order of comments help derive different themes
-        task_result: DerivedThemes = await sit.apply_task(task_input=task_input,
-                                                get_prompt=survey_task.prompt_messages,
-                                                result_class=survey_task.result_class)
-        pprint(json.loads(task_result.model_dump_json()))
-        results.append(task_result)
+        task_result: DerivedThemes = await sit.apply_task(task_input=task_input, 
+                                                          get_prompt=survey_task.prompt_messages, 
+                                                          result_class=survey_task.result_class)
+        # show the theme titles and descriptions
+        for theme in task_result.themes:
+            print(f"title: {theme.title}")
+            print(f"description: {theme.description}")
+            print(f"citations: {theme.citations}")
+            print()
+        # pprint(json.loads(task_result.model_dump_json()))
 
-    # combine the results - maybe I need to flatten the batches first and just feed this as DerivedThemes, not a batch
-    reduce_task_input = DerivedThemes(themes=[theme for derived_themes in results for theme in derived_themes.themes])
-    # reduce_task_input = DerivedThemesBatch(derived_themes=results)
+        results.extend(task_result.themes)
+
+    print(f"number of total themes across {shuffle_passes} passes: {len(results)}")
+
+    reduce_task_input = DerivedThemes(themes=results)
     reduce_task = CombineThemes(survey_question=survey_task.question)
+    print("combining results")
     final_task_result = await sit.apply_task(task_input=reduce_task_input,
                                             get_prompt=reduce_task.prompt_messages,
                                             result_class=reduce_task.result_class)
@@ -142,53 +152,93 @@ class CombineThemes(SurveyTaskProtocol):
         return DerivedThemes
 
     def prompt_messages(self, task_input: DerivedThemes) -> list[dict[str, str]]:
-        """Creates the messages for the theming prompt
-        Expects themes to be of form:
-        ('Themes:\n'
-        '\n'
-        'Theme title: Helpful Videos\n'
-        'Theme description: Many students appreciated the use of videos in the '
-        'course, finding them helpful for understanding complex concepts.\n'
-        '\n'
-        'Theme title: Clinical Applications\n'
-        'Theme description: Students found the clinical applications and real-world '
-        'examples particularly engaging and useful for understanding the material.\n'
-        '\n')
-        """
+        """Creates the messages for the theming prompt"""
 
         def format_themes(derived_themes:DerivedThemes) -> str:
             """Formats themes into a string"""
-            theme_str = "Themes:\n\n"
+            # theme_str = "Themes:\n\n"
+            theme_str = ""
             # flatten the list of derived themes in the batch
-            themes = [theme for theme in derived_themes.themes]
-            for theme in themes:
+            for theme in derived_themes.themes:
+                theme_str += f"<theme>\n"
                 theme_str += f"title: {theme.title}\n"
                 theme_str += f"description: {theme.description}\n"
-                theme_str += f"citations: {theme.citations}\n"
+                theme_str += f"citations:\n"
+                for citation in theme.citations:
+                    theme_str += f"  - {citation}\n"
+                theme_str += f"</theme>\n"
                 theme_str += "\n"
             return theme_str
 
-        system_message = f"""You are a highly-skilled assistant that works with themes \
-from student course feedback comments. A theme is a short phrase that summarizes \
-a piece of feedback that is expressed by multiple students. You respond only with a JSON array.
+#         system_message = f"""You are a highly-skilled assistant that works with themes \
+# from student course feedback comments. A theme is a short phrase that summarizes \
+# a piece of feedback that is expressed by multiple students. You respond only with a JSON array.
 
-You will be given a list of themes that were derived from student course feedback comments. \
-Each theme has a short title, a description, and a list of citations that are exact quotes supporting \
-the theme. These themes were derived from survey responses to the question: "{self.survey_question}" \
-Your task is to combine similar themes into a single theme. If you find two or more \
-themes that express a common idea, then combine them into a single theme with a title and \
-description. When combining themes, you should combine the citations from the themes into \
-a single list of citations. If any of the citations are duplicates, then remove the duplicates \
-when combining the citations. Do not alter the citations in any other way. 
+# You will be given a list of themes that were derived from student course feedback comments. \
+# Each theme has a short title, a description, and a list of citations that are exact quotes supporting \
+# the theme. These themes were derived from survey responses to the question: "{self.survey_question}" \
+# Your task is to combine similar themes. 
 
-Once you have done your combining, respond with a JSON array of theme objects. \
-Each theme object should have a title field, a description field, and a citations field."""
+# If there are two themes that have the same title, then combine them into a single theme. \
+# If there are two themes that have titles and descriptions that are very similar, then combine \
+# them into a single theme. For example, you might combine the themes "Helpful Videos" and \
+# "Videos were helpful" into a single theme with the title "Helpful Videos". Another example: you \
+# might combine "Clinical Applications" and "Real-world examples" into a single theme with the \
+# title "Clinical Applications". 
+
+# When combining themes, you can either create a new description or use one of the existing descriptions \
+# if it applies to both themes. 
+
+# When combining themes, you should combine the citations from the combined themes into \
+# a single list of citations. For example, if you are combining two themes and each had 3 citations, \
+# then the combined theme would have 6 citations. The only exception to that is if any of the citations \
+# in the combined theme are exact duplicates. In that case, remove one of the duplicate \
+# quotes. Do not alter the citations in any other way. 
+
+# Once you have done your combining, respond with a JSON array of theme objects, representing \
+# the combined themes. Each theme object should have a title field, a description field, and a citations field."""
+
+        alt_system_message = f"""You are an assistant who is highly skilled at working with \
+student feedback surveys. Your task is to thoroughly analyze and consolidate a list \
+of themes derived from student feedback on an online course. Each theme consists of a \
+'title', 'description', and a list of 'citations' (exact quotes from students). These \
+themes were derived from survey responses to the question: "{self.survey_question}". \
+Your primary focus is on merging themes that cover the same or very similar topics, \
+based on their titles and descriptions.
+
+First, identify similar themes. Review each theme and compare with others to find content \
+similarities. Look for themes with overlapping or complementary content, even if their titles \
+are not identical. For instance, 'Expert Instruction' and 'Expert Instructors' might \
+cover similar content from different angles and should be considered for merging. Similarly, \
+'Interactive Learning' and 'Hands-On Modules' may also overlap significantly in content. \
+Save your detailed reasoning from this first step for later output in a JSON object, under the \
+key 'step1_reasoning'.
+
+Next, merge and refine themes: Having identified similar themes:
+   - Combine their citations into one list, removing any duplicates.
+   - Create a new, consolidated title that captures the essence of the merged themes.
+   - Write a comprehensive description that encompasses all aspects of the themes being merged.
+
+Save the output of this step for later output in a JSON object, under the key \
+'step2_intermediate_themes', as a list of themes including the 'title', \
+'description', and 'citations' for each theme.
+
+Finally, look for any unique themes from the original list of themes that may have been lost \
+in the consolidation and, if necessary, update the consolidated list of themes to include \
+those. Save the final, consolidated list of themes for later output in a JSON object under the \
+key 'final_combined_themes' as a list of theme objects, each containing title, description, and citations.
+
+After you have completed all of these steps, present the final overall results in a \
+JSON object, including 'step1_reasoning', 'step2_intermediate_themes', and 'final_combined_themes'.
+
+You will now be presented with the original list of themes."""
 
         user_message = f"""{format_themes(task_input)}"""
 
         messages =  [  
-        {'role':'system', 'content': system_message},
-        {'role':'user', 'content': user_message}]
+            {'role':'system', 'content': alt_system_message},
+            {'role':'user', 'content': user_message}
+        ]
 
         return messages
 
@@ -196,4 +246,4 @@ Each theme object should have a title field, a description field, and a citation
     @property
     def result_class(self) -> Type[OpenAISchema]:
         """Returns the result class for theme derivation combining"""
-        return DerivedThemes
+        return ThemeConsolidation

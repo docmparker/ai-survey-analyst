d88f2433b056a700e38e0399892928a29c851d2f
Michael Parker <docmparker@gmail.com>
2024-02-01 13:35:45 -0500

    Add sentiment analysis task and add new single input task with logprobs to accommodate using logprobs

---
+++ src/survey_analysis/sentiment_analysis.py
@@ -0,0 +1,116 @@
+# from instructor.function_calls import OpenAISchema
+from .utils import OpenAISchema
+from .single_input_task import InputModel, SurveyTaskProtocol, CommentModel, apply_task_with_logprobs, LLMConfig
+from pydantic import Field, validate_arguments, model_validator
+from typing import Any, Type, Literal
+from functools import partial
+from . import batch_runner as br
+import openai.types.chat.chat_completion
+import numpy as np
+
+
+# Create the model - here we do it outside the class so it can also be used elsewhere if desired
+# without instantiating the class
+class SentimentAnalysisResult(OpenAISchema):
+    """Store the sentiment and reasoning for a survey comment"""
+    reasoning: str = Field("The comment had no content", description="The reasoning for the sentiment assignment")
+    sentiment: Literal["positive", "neutral", "negative"] | None = Field(None, description="The sentiment of the comment")
+    logprobs: openai.types.chat.chat_completion.ChoiceLogprobs | None = Field(None, description="The log probabilities for the sentiment assignment")
+    sentiment_logprobs_: list[dict[str, Any]] | None = Field(None, description="The log probabilities for just the sentiment token")
+
+    @property
+    def sentiment_logprobs(self) -> list[dict[str, Any]] | None:
+        """Returns the log probabilities and transformed (linear probs) 
+        for just the sentiment token. 
+        
+        Note that this logic caches the result so that it is only computed once.
+        Also note that this relies on the fact that 'positive', 'neutral', and 'negative'
+        are all single tokens with the gpt-4 tokenizer.
+        """
+
+        if self.logprobs and not self.sentiment_logprobs_:
+            def find_subsequence(seq, subseq):
+                sub_len = len(subseq)
+                for i in range(len(seq)):
+                    if seq[i:i+sub_len] == subseq:
+                        return i
+
+            tokens = self.logprobs.model_dump().get('content')
+            token_values = [token['token'] for token in tokens]
+            subsequence = ['sent', 'iment', '":', ' "']
+            index = find_subsequence(token_values, subsequence)
+
+            if index:
+                following_token = tokens[index + len(subsequence)]
+            else:
+                raise ValueError("Could not find the sentiment in the logprobs")
+
+            self.sentiment_logprobs_ = [{'token': token['token'], 'logprob': token['logprob'], 'linear_prob': np.round(np.exp(token['logprob'])*100,2)}
+                    for token in following_token['top_logprobs']]
+
+        return self.sentiment_logprobs_   
+
+
+class SentimentAnalysis(SurveyTaskProtocol):
+    """Class for sentiment analysis task"""
+    def __init__(self, question: str):
+        self.question = question
+
+    @property
+    def input_class(self) -> Type[InputModel]:
+        return CommentModel
+
+    def prompt_messages(self, task_input: CommentModel) -> list[dict[str, str]]:
+        """Creates the messages for the sentiment analysis prompt"""
+
+        delimiter = "####"
+        # question = "What could be improved about the course?"
+
+        system_message = f"""You are an assistant that determines the sentiment of \
+student course feedback comments.  You respond only with a JSON object.
+
+You will be provided with a comment from a student course feedback survey. \
+The comment will be delimited by {delimiter} characters. \
+Each original comment was in response to the question: "{self.question}". \
+Your task is to determine the sentiment of the comment and provide your reasoning for the sentiment. \
+
+Step 1: Reason through what the sentiment of the comment is and why.
+- Include your reasoning in the "reasoning" field.
+
+Step 2. Record the sentiment of the comment.
+- The sentiment MUST be one of three categories: "positive", "neutral", or "negative".
+- Include the sentiment in the "sentiment" field.
+
+Do your best. I will tip you $500 if you do an excellent job."""
+
+
+        user_message = f"""{delimiter}{task_input.comment}{delimiter}"""
+
+        messages =  [  
+        {'role':'system', 'content': system_message},
+        {'role':'user', 'content': user_message}]
+
+        return messages
+
+    @property
+    def result_class(self) -> Type[OpenAISchema]:
+        """Returns the result class for sentiment analysis"""
+        return SentimentAnalysisResult
+    
+
+@validate_arguments
+async def classify_sentiment(*, comments: list[str | None], question: str, goal_focus: str) -> list[OpenAISchema]:
+    """Classify the sentiment for each of a list of comments, based on a particular question 
+    
+    Returns a list of SentimentAnalysisResult objects
+    """
+
+    comments_to_test: list[CommentModel] = [CommentModel(comment=comment) for comment in comments]
+    survey_task: SurveyTaskProtocol = SentimentAnalysis(question=question)
+    sentiment_task = partial(apply_task_with_logprobs, 
+                      get_prompt=survey_task.prompt_messages, 
+                      result_class=survey_task.result_class,
+                      llm_config=LLMConfig(logprobs=True, top_logprobs=3))
+    extractions = await br.process_tasks(comments_to_test, sentiment_task)
+
+    return extractions
--- src/survey_analysis/single_input_task.py
+++ src/survey_analysis/single_input_task.py
@@ -49,7 +49,7 @@ class CommentModel(InputModel, OpenAISchema):
 # takes a task object and returns the messages for the prompt
 GetPrompt = Callable[[BaseModel], list[dict[str, str]]]
 
-# signature for running tasks that take a task object and return an OpenAISchema
+# signature for running tasks that take a task object and returns an OpenAISchema
 ApplyTask = Callable[[BaseModel, GetPrompt, OpenAISchema, LLMConfig | None], OpenAISchema] 
 
 
@@ -82,6 +82,45 @@ async def apply_task(task_input: InputModel,
     # result = result_class.from_response(response, mode=Mode.TOOLS)
     args = response.choices[0].message.tool_calls[0].function.arguments
     result = result_class.model_validate_json(args)
+
+    return result
+
+async def apply_task_with_logprobs(task_input: InputModel, 
+                     get_prompt: GetPrompt, 
+                     result_class: OpenAISchema, 
+                     llm_config: LLMConfig=None) -> OpenAISchema:
+    """Gets the result of applying an NLP task to a comment, list of comments, or some other unit or work."""
+    if llm_config is None:
+        llm_config = LLMConfig()
+
+    # if the task_input has no content (is a None equivalent), return early with
+    # an empty classification (filled in with defaults)
+    if task_input.is_empty():   # not comment_has_content(comment):
+        return result_class()
+    
+    # expect partial application of get_prompt if needs something like the tags_list
+    messages = get_prompt(task_input)
+    # fn_schema = result_class.openai_schema()
+
+    response = await aclient.chat.completions.create(
+        model=llm_config.model,
+        temperature=llm_config.temperature,
+        messages=messages,
+        # tools=[{"type": "function", "function": fn_schema}],
+        # tool_choice={"type": "function", "function": {"name": fn_schema['name']}},
+        response_format={"type": "json_object"},
+        logprobs=llm_config.logprobs,
+        top_logprobs=llm_config.top_logprobs
+    )
+
+    args = response.choices[0].message.content 
+
+    # take the json output and put it in the result class
+    result = result_class.model_validate_json(args) 
+
+    # add the logprobs to the result class
+    logprobs = response.choices[0].logprobs
+    result.logprobs = logprobs
 
     return result
 
